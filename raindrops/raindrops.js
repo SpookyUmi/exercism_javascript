03346d697f309253fc10706d8614fd36e5851dca
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2020-06-08 19:16:33 +0200

    other solution

--- raindrops/raindrops.js
+++ raindrops/raindrops.js
@@ -3,6 +3,24 @@
 // convenience to get you started writing code faster.
 //
 
+/* export const convert = (x) => {
+  let result = ''
+  if (x % 3 === 0) {
+    result += 'Pling'
+  }
+  if (x % 5 === 0) {
+    result += 'Plang'
+  }
+  if (x % 7 === 0) {
+    result += 'Plong'
+  }
+  if (!result) {
+    return String(x);
+  }
+
+  return result
+} */
+
 export const convert = (x) => {
   const pling = 'Pling';
   const plang = 'Plang';
