c43319ef20a026a763c9781bf9e2c842dc074856
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2020-06-10 11:16:55 +0200

    add finished solution

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -3,6 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRna = () => {
-
+export const toRna = (letters) => {
+  let result = '';
+  letters.split('').forEach(letter => {
+    if (letter === 'C') {
+      result += 'G'
+    }
+    if (letter === 'G') {
+      result += 'C'
+    }
+    if (letter === 'T') {
+      result += 'A'
+    }
+    if (letter === 'A') {
+      result += 'U'
+    }
+    // console.log('result', result)
+  });
+  return result;
 };
