296c63c4407c2276d4b5976a71bc11c7a0677892
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2020-06-17 19:49:44 +0200

    kos est relou

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -4,16 +4,5 @@
 //
 
 export const isPangram = (string) => {
-  if (!string) return false
-  let alphabet = [];
-  let upStart = 'A'.charCodeAt(0);
-  let upEnd = 'Z'.charCodeAt(0);
-  for (let e = upStart; e <= upEnd; e++) {
-    alphabet.push(e);
-  }
-  const codeArray = string.toUpperCase().split('').map(letter => letter.charCodeAt(0))
-  let findMatch = (codeA, alph) => alph.every(el => codeA.includes(el));
-
-  if (findMatch(codeArray, alphabet)) return true
-  return false
+  return [...new Set(string.match(/[a-z'".]/g))].length === 26
 };
