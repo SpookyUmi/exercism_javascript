6624ef1341edc357a386a202df53eebaceaaf6f4
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2020-06-17 20:23:56 +0200

    isogram initialization

---
+++ isogram/isogram.js
@@ -0,0 +1,11 @@
+//
+// This is only a SKELETON file for the 'Isogram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const isIsogram = (string) => {
+  if (string === '') return true
+
+  const lettersArray = string.toUpperCase().split('')//.map(letter => letter.charCodeAt(0))
+
+};
