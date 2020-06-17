3bb0f59e2985d74a364484db9c7d95e9d22666ca
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2020-06-17 13:03:06 +0200

    submit my solution

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -5,21 +5,15 @@
 
 export const isPangram = (string) => {
   if (!string) return false
-     // ici tu pourrais faire un return en une seule ligne, genre if(!string) return false // OK merci (:
   let alphabet = [];
   let upStart = 'A'.charCodeAt(0);
   let upEnd = 'Z'.charCodeAt(0);
   for (let e = upStart; e <= upEnd; e++) {
-    alphabet.push(String(e));
+    alphabet.push(e);
   }
-  let lowStart = 'a'.charCodeAt(0);
-  let lowEnd = 'z'.charCodeAt(0);
-  for (let e = lowStart; e <= lowEnd; e++) { // nice d'avoir pensé au let ici
-    alphabet.push(String(e));
-  }
-  console.log(alphabet)
-  const stringArray = string.split('')
-  console.log(stringArray);
-  let codeArray = [stringArray.forEach(e => e.charCodeAt(0))]
-  console.log(codeArray);
+  const codeArray = string.toUpperCase().split('').map(letter => letter.charCodeAt(0))
+  let findMatch = (codeA, alph) => alph.every(el => codeA.includes(el));
+
+  if (findMatch(codeArray, alphabet)) return true
+  return false
 };
