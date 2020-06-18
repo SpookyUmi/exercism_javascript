91d888c7d6d82bd486826bdb322cbb1a2bedbcdc
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2020-06-18 19:32:17 +0200

    tryouts

--- isogram/isogram.js
+++ isogram/isogram.js
@@ -6,6 +6,17 @@
 export const isIsogram = (string) => {
   if (string === '') return true
 
-  const lettersArray = string.toUpperCase().split('')//.map(letter => letter.charCodeAt(0))
-
+  const lettersArray = string.toLowerCase().split('')//.map(letter => letter.charCodeAt(0))
+  return [...new Set(string.match(/[a-z]/g))]//.length
+    /* var valuesSoFar = Object.create(null);
+    for (var i = 0; i < lettersArray.length; ++i) {
+      var value = lettersArray[i];
+      if (value in valuesSoFar) {
+        return false;
+      }
+      valuesSoFar[value] = false;
+    }
+    return true; */
 };
+
+//return [...new Set(string.match(/[a-z'".]/g))].length === 26
