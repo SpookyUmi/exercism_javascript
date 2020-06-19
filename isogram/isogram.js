6682b18fc138796d66f5a90b7cd47eeb57ebbcc6
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2020-06-19 16:41:22 +0200

    a first try for the isogram exercise

--- isogram/isogram.js
+++ isogram/isogram.js
@@ -6,17 +6,9 @@
 export const isIsogram = (string) => {
   if (string === '') return true
 
-  const lettersArray = string.toLowerCase().split('')//.map(letter => letter.charCodeAt(0))
-  return [...new Set(string.match(/[a-z]/g))]//.length
-    /* var valuesSoFar = Object.create(null);
-    for (var i = 0; i < lettersArray.length; ++i) {
-      var value = lettersArray[i];
-      if (value in valuesSoFar) {
-        return false;
-      }
-      valuesSoFar[value] = false;
-    }
-    return true; */
+  const sentence = string.match(/\w/gi);
+  const regMatch = [...new Set(string.match(/\w/gi))];
+  if (sentence === regMatch) {
+    return true
+  } return false
 };
-
-//return [...new Set(string.match(/[a-z'".]/g))].length === 26
