111af5ebb1ef21c227747013f8d6c86bcd9b317f
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2020-07-17 12:33:58 +0200

    final solution

--- isbn-verifier/isbn-verifier.js
+++ isbn-verifier/isbn-verifier.js
@@ -4,12 +4,19 @@
 //
 
 export const isValid = (number) => {
-  if (number.length !== 10) return false
   const regMatch = number.match(/[\dx]/gi)
-  if (regMatch.findIndex('X') !== regMatch.length - 1 /* = 9 */) return false
+  //? check if number is at least present with `number.indexOf('X') > 0`
+  // if (number.indexOf('X') > 0 && number.indexOf('X') < regMatch.length - 1) return false
+  //? regex returns `null` if it does not match any pattern, and therefore, regMatch.length throws an error
+  if (regMatch === null || regMatch.length !== 10) return false
   let sum = 0;
   for (let index = 10; index > 1; index--) {
-    sum += index;
+    sum += index * Number(regMatch[regMatch.length - index])
   }
-  return sum
+  if (regMatch[regMatch.length - 1] === 'X') {
+    sum += 10;
+  } else {
+    sum += Number(regMatch[regMatch.length - 1])
+  }
+  return sum % 11 === 0
 };
